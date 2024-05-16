30c3cdccbc7c21771750c8c4a4bef24e282cb7d5
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2024-05-16 18:57:29 -0300

    cambios

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
 
 	public static void main(String args[]) {
 		Servicios servicios = new Servicios("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
-	
+		System.out.println(servicios.getTarea("T1"));
 	}
 }
