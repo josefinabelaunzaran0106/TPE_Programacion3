a5a6040c02837101f5742b2a61ac5ab85d293f49
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2024-06-01 11:06:17 -0300

    cambios

--- tpe/Main.java
+++
@@ -1,9 +0,0 @@
-package tpe;
-
-public class Main {
-
-	public static void main(String args[]) {
-		Servicios servicios = new Servicios("./tpe/datasets/Procesadores.csv", "./tpe/datasets/Tareas.csv");
-		System.out.println(servicios.getTareasPrioridadIn(8,80));
-	}
-}
